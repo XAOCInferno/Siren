45af53b464de95a75fc992f70bf81e4679fab6de
LeEspresso <TestXBot@gmail.com>
2026-03-12 20:01:56 +0000

    [~] Generalised BoardSystem, so that we can have different layers of objects on the board (for piece layer above tile layer)

--- Assets/Scripts/Gameplay/BoardController.cs
+++ Assets/Scripts/Gameplay/BoardController.cs
@@ -7,34 +7,38 @@ using UnityEngine;
 
 namespace Gameplay
 {
-    public static class BoardSystem
+    /// <summary>
+    /// System handling a grid with items on it.
+    /// T: Represents which layer we are occupying. EG: Tile layer, Piece layer. Items on different layers will not block each other and will need further checks if this is required. 
+    /// </summary>
+    public static class BoardSystem<T> where T : UnityEngine.Object
     {
-        private static Tile[,] _tiles;
+        private static T[,] _items;
 
         /// <summary>
         /// Sets the size of the grid
         /// </summary>
-        /// <param name="width">Number of tiles in x direction.</param>
-        /// <param name="height">Number of tiles in y direction.</param>
+        /// <param name="width">Number of items in x direction.</param>
+        /// <param name="height">Number of items in y direction.</param>
         public static void SetGridSize(int width, int height)
         {
-            _tiles = new Tile[width, height];
+            _items = new T[width, height];
         }
 
         /// <summary>
-        /// Gets the tile at grid coordinates, or returns null if there is no tile (or passed values are invalid)
+        /// Gets the item at grid coordinates, or returns null if there is no item (or passed values are invalid)
         /// </summary>
-        /// <param name="gridCoordinates">Coordinate of the tile.</param>
-        /// <returns>Returns Tile or Null.</returns>
+        /// <param name="gridCoordinates">Coordinate of the item.</param>
+        /// <returns>Returns T or Null.</returns>
         [CanBeNull]
-        public static Tile GetTileOnGrid(Vector2Int gridCoordinates)
+        public static T GetItemOnGrid(Vector2Int gridCoordinates)
         {
             //Check we have valid array index
-            if (gridCoordinates.x >= 0 && gridCoordinates.x < _tiles.GetLength(0)
-                                       && gridCoordinates.y >= 0 && gridCoordinates.y < _tiles.GetLength(1))
+            if (gridCoordinates.x >= 0 && gridCoordinates.x < _items.GetLength(0)
+                                       && gridCoordinates.y >= 0 && gridCoordinates.y < _items.GetLength(1))
             {
-                //Valid index, return Tile or Null depending on if we have anything here
-                return _tiles[gridCoordinates.x, gridCoordinates.y];
+                //Valid index, return T or Null depending on if we have anything here
+                return _items[gridCoordinates.x, gridCoordinates.y];
             }
 
             //Invalid index, return null
@@ -43,61 +47,61 @@ namespace Gameplay
 
 
         /// <summary>
-        /// Gets an array containing all the tiles that fit within a Square area
+        /// Gets an array containing all the items that fit within a Square area
         /// </summary>
-        /// <param name="center">Coordinate of central tile, middle of the square. For even numbers (eg 4 cell square) this will be weighted to the central bottom left.</param>
-        /// <param name="size">How large the square area is. Indicates number of tiles in both x and y directions (eg: value of 2 is a 2x2 square). Value of 1 returns only central tile.</param>
-        /// <returns>Returns Tiles.</returns>
-        public static Tile[] GetTilesInSquare(Vector2Int center, int size)
+        /// <param name="center">Coordinate of central item, middle of the square. For even numbers (eg 4 cell square) this will be weighted to the central bottom left.</param>
+        /// <param name="size">How large the square area is. Indicates number of items in both x and y directions (eg: value of 2 is a 2x2 square). Value of 1 returns only central item.</param>
+        /// <returns>Returns Array of T.</returns>
+        public static T[] GetItemsInSquare(Vector2Int center, int size)
         {
-            //Cannot get tiles in square when size is <=0, smallest square is 1 (center tile only)
+            //Cannot get items in square when size is <=0, smallest square is 1 (center item only)
             Assert.Greater(size, 0);
 
-            //If size 1, then just get the center tile
+            //If size 1, then just get the center item
             if (size == 1)
             {
-                return new Tile[1] { GetTileOnGrid(center) };
+                return new T[1] { GetItemOnGrid(center) };
             }
 
             int halfSize = 1 + Mathf.CeilToInt(size / 2.0f);
-            return GetTilesInRect(center, size, size);
+            return GetItemsInRect(center, size, size);
         }
 
         /// <summary>
-        /// Gets an array containing all the tiles that fit within a Rectangle area
+        /// Gets an array containing all the items that fit within a Rectangle area
         /// </summary>
-        /// <param name="center">Coordinate of central tile, middle of the square. For even numbers (eg 4 cell square) this will be weighted to the central bottom left.</param>
-        /// <param name="width">Number of tiles in x direction.</param>
-        /// <param name="height">Number of tiles in y direction.</param>
-        /// <returns>Returns Tiles.</returns>
-        public static Tile[] GetTilesInRect(Vector2Int center, int width, int height)
+        /// <param name="center">Coordinate of central item, middle of the square. For even numbers (eg 4 cell square) this will be weighted to the central bottom left.</param>
+        /// <param name="width">Number of items in x direction.</param>
+        /// <param name="height">Number of items in y direction.</param>
+        /// <returns>Returns Array of T.</returns>
+        public static T[] GetItemsInRect(Vector2Int center, int width, int height)
         {
-            //Ensure we have valid size, size of 0 will never provide any tiles
+            //Ensure we have valid size, size of 0 will never provide any items
             Assert.Greater(height, 0);
             Assert.Greater(width, 0);
 
-            //Tiles we will return
-            List<Tile> listOfTiles = new();
+            //Items we will return
+            List<T> listOfItems = new();
 
             //Area, Half height
             int area = height * width;
             int halfHeight = height / 2;
             int halfWidth = width / 2;
 
-            //Loop over the area and add tiles to our return list
+            //Loop over the area and add items to our return list
             int xOffset = 0;
             int yOffset = 0;
             for (int i = 0; i < area; i++)
             {
-                //Get grid coordinate and tile at that location
+                //Get grid coordinate and item at that location
                 Vector2Int gridLocation =
                     new Vector2Int(xOffset + center.x - halfWidth, yOffset + center.y - halfHeight);
 
-                //Try get the tile, then add it if we succeeded. Failure suggests tile is outside the play area
-                Tile nextTile = GetTileOnGrid(gridLocation);
-                if (nextTile)
+                //Try get the item, then add it if we succeeded. Failure suggests item is outside the play area
+                T nextItem = GetItemOnGrid(gridLocation);
+                if (nextItem)
                 {
-                    listOfTiles.Add(nextTile);
+                    listOfItems.Add(nextItem);
                 }
 
                 //Increment, reset X if we've reached width
@@ -110,25 +114,25 @@ namespace Gameplay
             }
 
             //Return our finished array
-            return listOfTiles.ToArray();
+            return listOfItems.ToArray();
         }
 
         /// <summary>
-        /// Gets an array containing all the tiles that fit within a Circular area
+        /// Gets an array containing all the items that fit within a Circular area
         /// </summary>
-        /// <param name="center">Coordinate of central tile, middle of the circle. Unlike square, this will ALWAYS be the central tile.</param>
-        /// <param name="radius">How large should circle be. Value of 1 is the central tile + 1 in every direction, increasing by +1 tile in every direction.</param>
-        /// <returns>Returns Tiles.</returns>
-        public static Tile[] GetTilesInCircle(Vector2Int center, int radius)
+        /// <param name="center">Coordinate of central item, middle of the circle. Unlike square, this will ALWAYS be the central item.</param>
+        /// <param name="radius">How large should circle be. Value of 1 is the central item + 1 in every direction, increasing by +1 item in every direction.</param>
+        /// <returns>Returns Array of T.</returns>
+        public static T[] GetItemsInCircle(Vector2Int center, int radius)
         {
-            //Tiles to return
-            List<Tile> listOfTiles = new();
+            //Items to return
+            List<T> listOfItems = new();
 
             //Y Bounds of circle
             int top = Mathf.CeilToInt(center.y - radius),
                 bottom = Mathf.FloorToInt(center.y + radius);
 
-            //Calculate all tiles in circle
+            //Calculate all items in circle
             for (int y = top; y <= bottom; y++)
             {
                 int dy = y - center.y;
@@ -137,30 +141,30 @@ namespace Gameplay
                     right = Mathf.FloorToInt(center.x + dx);
                 for (int x = left; x <= right; x++)
                 {
-                    //Get our tile, add it to data if valid
-                    Tile tile = GetTileOnGrid(new Vector2Int(x, y));
-                    if (tile)
+                    //Get our item, add it to data if valid
+                    T item = GetItemOnGrid(new Vector2Int(x, y));
+                    if (item)
                     {
-                        listOfTiles.Add(tile);
+                        listOfItems.Add(item);
                     }
                 }
             }
 
-            return listOfTiles.ToArray();
+            return listOfItems.ToArray();
         }
 
         /// <summary>
-        /// Gets all tiles that lie inside an Arc (Curve)
+        /// Gets all items that lie inside an Arc (Curve)
         /// </summary>
-        /// <param name="center">Coordinate of central tile, origin of the arc.</param>
-        /// <param name="arcLength">Length to the end of the arc, length = 1 + n tiles in direction.</param>
+        /// <param name="center">Coordinate of central item, origin of the arc.</param>
+        /// <param name="arcLength">Length to the end of the arc, length = 1 + n items in direction.</param>
         /// <param name="dirAngle">Which direction is angle pointing? 0-Left, 0.25-Up, 0.5-Right, 0.75-Down.</param>
         /// <param name="curveAngle">How large will the arc be? 1-HalfCircle, 0.5-QuarterCircle, etc.</param>
-        /// <returns>Returns an array of tiles that lie inside the arc defined in params.</returns>
-        public static Tile[] GetTilesInArc(Vector2Int center, int arcLength, float dirAngle, float curveAngle)
+        /// <returns>Returns an array of items that lie inside the arc defined in params.</returns>
+        public static T[] GetItemsInArc(Vector2Int center, int arcLength, float dirAngle, float curveAngle)
         {
             //Return list
-            List<Tile> listOfTiles = new();
+            List<T> listOfItems = new();
 
             //Convert to useable correctly formatted values for formula
             dirAngle = Math.Clamp(1 - dirAngle, 0, 1);
@@ -174,7 +178,7 @@ namespace Gameplay
             int bottom = Mathf.FloorToInt(center.y + arcLength);
 
             //~TODO: Remove duplicated code?
-            //Calculate all tiles in circle
+            //Calculate all items in circle
             for (int y = top; y <= bottom; y++)
             {
                 int dy = y - center.y;
@@ -187,32 +191,32 @@ namespace Gameplay
                     Vector3 dirCenterToPoint = (center - new Vector2(x, y)).normalized;
                     float dot = Vector2.Dot(fwd, dirCenterToPoint);
 
-                    //If angle is valid add the tile to our output list
+                    //If angle is valid add the item to our output list
                     if (dot >= curveAngle)
                     {
-                        Tile tile = GetTileOnGrid(new Vector2Int(x, y));
-                        if (tile)
+                        T item = GetItemOnGrid(new Vector2Int(x, y));
+                        if (item)
                         {
-                            listOfTiles.Add(tile);
+                            listOfItems.Add(item);
                         }
                     }
                 }
             }
 
             //Return, converted to array
-            return listOfTiles.ToArray();
+            return listOfItems.ToArray();
         }
 
         /// <summary>
-        /// Sets the tile at a specific grid coordinate, performed during board setup
+        /// Sets the item at a specific grid coordinate, performed during board setup
         /// </summary>
         /// <param name="gridCoordinates">Coordinate to set.</param>
-        /// <param name="tile">The tile.</param>
-        public static void SetTileOnGrid(Vector2Int gridCoordinates, Tile tile)
+        /// <param name="item">The item.</param>
+        public static void SetItemOnGrid(Vector2Int gridCoordinates, T item)
         {
-            //Ensure we are not overwriting a tile, that is not supported. If we want to replace a tile, functionality should be added for that
-            Assert.IsNull(_tiles[gridCoordinates.x, gridCoordinates.y]);
-            _tiles[gridCoordinates.x, gridCoordinates.y] = tile;
+            //Ensure we are not overwriting a item, that is not supported. If we want to replace a item, functionality should be added for that
+            Assert.IsNull(_items[gridCoordinates.x, gridCoordinates.y]);
+            _items[gridCoordinates.x, gridCoordinates.y] = item;
         }
     }
 
@@ -231,6 +235,14 @@ namespace Gameplay
 
         private void Start()
         {
+            CreateBoardLayout();
+        }
+
+        /// <summary>
+        /// Creates the board, filling it with tiles.
+        /// </summary>
+        protected void CreateBoardLayout()
+        {
             //Ensure prefab is valid
             if (!tilePrefab)
             {
@@ -239,7 +251,7 @@ namespace Gameplay
             }
 
             //Create tiles array
-            BoardSystem.SetGridSize(boardWidth, boardHeight);
+            BoardSystem<Tile>.SetGridSize(boardWidth, boardHeight);
 
             //Iterate over and assign tiles to array
             int numberOfTiles = boardWidth * boardHeight;
@@ -263,7 +275,7 @@ namespace Gameplay
                 tile.SetGridLocation(new Vector2Int(currentX, currentY));
 
                 //Add to data
-                BoardSystem.SetTileOnGrid(gridCoordinates, tile);
+                BoardSystem<Tile>.SetItemOnGrid(gridCoordinates, tile);
 
                 //Set scale
                 Transform tileTransform = tileObject.GetComponent<Transform>();
