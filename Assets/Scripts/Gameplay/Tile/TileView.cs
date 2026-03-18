6a48b79297003f66d56e85f125f4a7c9cc0c2d51
LeEspresso <TestXBot@gmail.com>
2026-03-18 14:42:37 +0000

    [~] TileView implements IStateObject

--- Assets/Scripts/Gameplay/Tile/TileView.cs
+++ Assets/Scripts/Gameplay/Tile/TileView.cs
@@ -1,8 +1,32 @@
+using NUnit.Framework;
 using UnityEngine;
+using Utils.StateMachine;
 
 namespace Gameplay.Tile
 {
-    public class TileView : MonoBehaviour
+    public class TileView : MonoBehaviour, IStateObject<ETileState>
     {
+        protected TileObject tileObject;
+
+        private void Awake()
+        {
+            tileObject = gameObject.GetComponent<TileObject>();
+            Assert.NotNull(tileObject);
+            ListenToStateChangedEvent();
+        }
+
+        public void ListenToStateChangedEvent()
+        {
+            tileObject.GetState().GetStateMachine().ListenToStateChangedCallback(this);
+        }
+
+        public int OnStateChanged(EnumStateMachine<ETileState>.StateChangedEventPayload payload)
+        {
+            switch (payload.newState)
+            {
+            }
+
+            return 0;
+        }
     }
 }
