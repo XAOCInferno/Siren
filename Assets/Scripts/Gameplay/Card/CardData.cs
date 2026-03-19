f33bd6d0c28b04fbde218f8384ab7969f9a08ab9
LeEspresso <TestXBot@gmail.com>
2026-03-19 01:25:14 +0000

    [~] Remove unused Ids

--- Assets/Scripts/Gameplay/Card/CardData.cs
+++ Assets/Scripts/Gameplay/Card/CardData.cs
@@ -7,16 +7,13 @@ namespace Gameplay.Card
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Gameplay/Card", order = 1)]
     public class CardData : ScriptableObject
     {
-        [SerializeField] private int cardID;
         [SerializeField] private ECardType type;
 
         [CanBeNull] [SerializeField] private PieceData associatedPieceData;
 
         //View
         [SerializeField] private CardViewModelData viewData;
-
-
-        public int GetCardID() => cardID;
+        
         public ECardType GetCardType() => type;
         public PieceData GetAssociatedPieceData() => associatedPieceData;
         public CardViewModelData GetViewData() => viewData;
--- Assets/Scripts/Gameplay/Piece/PieceData.cs
+++ Assets/Scripts/Gameplay/Piece/PieceData.cs
@@ -5,11 +5,8 @@ namespace Gameplay
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Gameplay/Piece", order = 2)]
     public class PieceData : ScriptableObject
     {
-        [SerializeField] private int pieceID;
-
         public PieceData()
         {
-            pieceID = 0;
         }
     }
 }
